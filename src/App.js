cc78f3e06f3cde06bbcf652a2b5d8dcfe2dcc9bf
Clife-Developer <clife.united@gmail.com>
2021-02-06 19:27:51 +0200

    main component modified

--- src/App.js
+++ src/App.js
@@ -2,10 +2,30 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  co
+  const userData=[{id:1,name:'Clife',username:'Mhlongo'}]
+  const initialUserData=[{id:null,name:'',username:''}]
+
+  const [users,setUsers]= useState(userData)
+  const [currentUser,setCurrentUser]=useState(initialUserData)
+  const [editing,setEditing]=useState(false)
   return (
-    <div className="App">
-        <h1 className="bg-primary">Helo</h1>
+    <div className="container">
+        <h1>REACT HOOKS CRUDE APP</h1>
+        <div className="flex-row">
+          <div className="flex-large">
+            {editing ? (
+              <>
+                <h1>Edit User</h1>
+                <EditUser/>
+              </>
+            ):(
+              <>
+                <h1>Add User</h1>
+                <AddUser/>
+              </>
+            )}
+          </div>
+        </div>
     </div>
   );
 }
