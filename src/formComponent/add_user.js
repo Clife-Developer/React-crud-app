6746f9e8e8dcee3e6eca6f55f831841796e9a630
Clife-Developer <clife.united@gmail.com>
2021-02-06 21:36:45 +0200

    states created,user input modified

--- src/formComponent/add_user.js
+++ src/formComponent/add_user.js
@@ -1,15 +1,30 @@
-import React from 'react'
+import React,{useState} from 'react'
 
-function AddUserr(props) {
+function AddUser(props) {
      const initialUserState=[{id:null,name:'',username:''}]
      const [user,setUser]= useState(initialUserState)
+
+     const HandleOnChange=(e)=>{
+      const {name,value}=e.target;
+      setUser({
+          ...user,[name]:value
+      })
+     }
+
+      const HandleSubmit=(e)=>{
+         e.preventDefault();
+         if(user.name && user.username){
+             props.addUser(user);
+             setUser(initialUserState)
+         } 
+     }
     return (
-       <form>
+       <form onSubmit={HandleSubmit}>
            <label htmlFor="Name">Name</label>
-           <input type="text" name="name" id="Name" value={user.name} onClick={HandleOnChange}/>
+           <input type="text" name="name" id="Name" value={user.name} onChange={HandleOnChange}/>
 
            <label htmlFor="Name">Username</label>
-           <input type="text" name="name" id="Username" value={user.username} onClick={HandleOnChange}/>
+           <input type="text" name="username" id="Username" value={user.username} onChange={HandleOnChange}/>
 
            <button type="submit">Add</button>
        </form>
--- src/formComponent/edit_user.js
+++ src/formComponent/edit_user.js
@@ -1,15 +1,32 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react'
 
 function EditUser(props) {
-     const initialUserState=[{id:null,name:'',username:''}]
-     const [users,setUsers]= useState(initialUserState)
+    
+     const [user,setUser]= useState(props.currentUser)
+
+     const HandleOnChange=(e)=>{
+      const {name,value}=e.target;
+      setUser({
+          ...user,[name]:value
+      })
+     }
+     
+     useEffect(() => {
+         setUser(props.currentUser)
+        return()=>null
+     }, [props])
+
+     const HandleSubmit=(e)=>{
+         e.preventDefault();
+          props.updateUser(user.id, user)
+        }
     return (
-       <form>
+       <form onSubmit={HandleSubmit}>
            <label htmlFor="Name">Name</label>
-           <input type="text" name="name" id="Name" value={} onClick={HandleOnChange}/>
+           <input type="text" name="name" id="Name" value={user.name} onChange={HandleOnChange}/>
 
            <label htmlFor="Name">Username</label>
-           <input type="text" name="name" id="Username" value={} onClick={HandleOnChange}/>
+           <input type="text" name="username" id="Username" value={user.username} onChange={HandleOnChange}/>
 
            <button type="submit">Edit</button>
        </form>
